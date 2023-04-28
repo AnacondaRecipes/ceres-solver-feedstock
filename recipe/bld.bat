e90a406bb013c2fccf094cd695f4656546d07ce5
Serhii Kupriienko <skupriienko@anaconda.com>
2023-04-28 16:48:42 +0300

    Use ninja on win

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,6 +1,13 @@
-mkdir build_ && cd build_
+:: cmd
+echo "Building %PKG_NAME%."
 
-cmake -G "NMake Makefiles" ^
+mkdir build_ && cd build_
+if errorlevel 1 exit /b 1
+
+:: Generate the build files.
+echo "Generating the build files..."
+cmake .. %CMAKE_ARGS% ^
+    -G"Ninja" ^
     -DCMAKE_PREFIX_PATH=%LIBRARY_PREFIX% ^
     -DCMAKE_INSTALL_PREFIX=%LIBRARY_PREFIX% ^
     -DCMAKE_BUILD_TYPE=Release ^
@@ -8,7 +15,21 @@ cmake -G "NMake Makefiles" ^
     -DBUILD_EXAMPLES=OFF ^
     -DBUILD_TESTING=OFF ^
     ..
-if errorlevel 1 exit 1
+if errorlevel 1 exit /b 1
 
-cmake --build . --config Release --target install
-if errorlevel 1 exit 1
+
+:: Build.
+echo "Building..."
+ninja -j%CPU_COUNT%
+if errorlevel 1 exit /b 1
+
+
+:: Install.
+echo "Installing..."
+ninja install
+if errorlevel 1 exit /b 1
+
+
+:: Error free exit.
+echo "Error free exit!"
+exit 0
